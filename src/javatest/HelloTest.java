a9e93f3555c5b9452ba8bab13f084c172273cd60
wqs <857227746@qq.com>
2018-07-26 14:00:56 +0800

    修改输出

--- src/javatest/HelloTest.java
+++ src/javatest/HelloTest.java
@@ -1,10 +1,10 @@
-package java;
+package javatest;
 
 /**
  * Created by wqs on 2018/7/26.
  */
 public class HelloTest {
     public static void main(String [] arg){
-        System.out.print("test connection github");
+        System.out.println("test connection github");
     }
 }
