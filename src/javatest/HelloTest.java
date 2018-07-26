dc858584012ec89225f7c84290ba9362794afa53
wqs <857227746@qq.com>
2018-07-26 11:32:05 +0800

    提交测试代码

---
+++ src/javatest/HelloTest.java
@@ -0,0 +1,10 @@
+package java;
+
+/**
+ * Created by wqs on 2018/7/26.
+ */
+public class HelloTest {
+    public static void main(String [] arg){
+        System.out.print("test connection github");
+    }
+}
